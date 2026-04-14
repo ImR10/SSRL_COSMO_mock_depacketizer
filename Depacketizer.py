fe64fd6af61fa540a2c2d422ebcdd90663e55109
Syed Hoque <srh33227@uga.edu>
2026-04-14 09:31:47 -0400

    recent changes

--- Depacketizer.py
+++ Depacketizer.py
@@ -1,8 +1,24 @@
 # responsible for managing the different virtual channels by creating the TransferFrame objects
+import struct
 
 def main():
     print("SSRL COSMO Mock Depacketizer...")
-    print("Parsing...")
+    SPACE_DATA = "mission_data.bin"
+
+    try:
+        frame_size = 1024
+        with open(SPACE_DATA, "rb") as f:
+            while True:
+                frame = f.read(frame_size)
+                transfer_frame = TransferFrame()     
+                if not frame:
+                    break
+                
+
+    except FileNotFoundError:
+        print(f"Error: File {SPACE_DATA} NOT FOUND !!!")
+        
+
 
 
 if __name__ == "__main__":
