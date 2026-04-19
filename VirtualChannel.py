ee756c51c6e0b97bc70fe2f7a4f378339f914951
ImR10 <your-email@example.com>
2026-04-19 12:38:03 -0400

    added virtual channel changes

--- VirtualChannel.py
+++ VirtualChannel.py
@@ -1,2 +1,11 @@
 # responsible for slicing the buffer based on TFPH
 
+class VirtualChannel():
+    def __init__(self, vcid):
+        self.vcid = vcid
+        self.packet_buffer = bytearray()
+        self.target_packet_length = 0
+
+    def process_frame(self):
+        if (self.packet_buffer):
+            self.packet_buffer.append()
